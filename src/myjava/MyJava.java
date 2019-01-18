484eb9a9a097f3f636396d6015bdacf578916256
yusufdonmez <cengyusuf@gmail.com>
2019-01-19 01:27:25 +0300

    first commit for java tutorial

---
+++ src/myjava/MyJava.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myjava;
+
+/**
+ *
+ * @author yusuf
+ */
+public class MyJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Merhaba Ders");
+        insan Ali = new insan();
+        Ali.boy = 180;
+        Ali.kilo = 60;
+        Ali.yas = 22;
+        System.out.println("Alini yası: "+Ali.yas+" Alini kilosu: "+Ali.kilo+" ali boy: "+Ali.boy);
+        insan Veli = new insan();
+        Veli.boy=170;
+        Veli.yas=25;
+        Veli.kilo=55;
+        System.out.println("Velini yası: "+Veli.yas+" Velini kilosu: "+Veli.kilo+" ali boy: "+Veli.boy);
+        
+        
+    }
+    
+}
---
+++ src/myjava/insan.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myjava;
+
+/**
+ *
+ * @author yusuf
+ */
+public class insan {
+    int boy;
+    int kilo;
+    int yas;
+    void yemek(){
+        kilo++;
+    }
+}
